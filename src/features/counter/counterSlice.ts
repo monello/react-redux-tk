ba539f4e914c8e23ed7fc2e13862d86ec9602e9d
Morné Louw <louw.morne@gmail.com>
2022-07-21 17:03:20 +0200

    feat: implement the counter slice in a new Counter component

--- src/features/counter/counterSlice.ts
+++ src/features/counter/counterSlice.ts
@@ -1,6 +1,10 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-const initialState = {
+interface CounterState {
+    count: number;
+}
+
+const initialState: CounterState = {
     count: 0
 };
 
